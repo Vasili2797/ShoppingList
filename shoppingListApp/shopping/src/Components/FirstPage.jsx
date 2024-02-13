43521d9a1f66ff4c8c40a5dfef2942de030fb795
Vasili2797 <nashvilliv1@gmail.com>
2024-02-13 18:21:03 -0500

    The delete button works. Checkbox was added

--- shoppingListApp/shopping/src/Components/FirstPage.jsx
+++ shoppingListApp/shopping/src/Components/FirstPage.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import ProductsPage from "./ProductsPage";
 
 const FirstPage = () => {
   const [products, setProducts] = useState([]);
   const [product, setProduct] = useState("");
   const [line, setLine] = useState("");
-  const [checkedProducts, setCheckedProducts] = useState([]);
+  const [deletedProducts, setDeletedProducts] = useState([]);
 
   const handleInputChange = (e) => {
     setProduct(e.target.value);
@@ -19,12 +19,22 @@ const FirstPage = () => {
   };
 
   const handleDelete = (id) => {
-    if (checkedProducts.id === products.id) {
+    if (deletedProducts.id === products.id) {
       products.splice(id, 1);
     }
   };
 
-  console.log("The deleted Products: " + checkedProducts);
+  const strikeThroughProduct = () => {
+    const helloWorld = ProductsPage.checkedElementFunction();
+    console.log(
+      "I am consoling from firstPage and the variable is helloworld " +
+        helloWorld
+    );
+    // if (checkedElementFunction === products.id) {
+    // }
+  };
+
+  console.log("The deleted Products: " + deletedProducts);
 
   return (
     <>
@@ -43,24 +53,22 @@ const FirstPage = () => {
       <ul>
         {products.map((product1, index1) => {
           console.log(index1);
+          console.log("The products:  of te product 1 "+product1);
           return (
             <>
-              
               <ProductsPage
                 id={index1}
                 key={index1}
                 prod={product1}
-                isProductDeleted={checkedProducts.includes(index1)}
+                isProductDeleted={deletedProducts.includes(index1)}
                 completed={false}
-                
                 action={
-                  
                   <div className="input-group">
                     <button
                       // id={index1}
                       className="btn btn-danger ms-2"
                       onClick={() => {
-                        setCheckedProducts([...checkedProducts, index1]);
+                        setDeletedProducts([...deletedProducts, index1]);
                         handleDelete(index1);
                       }}
                     >
