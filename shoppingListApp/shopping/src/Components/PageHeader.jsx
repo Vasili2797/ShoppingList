3418473eb131ec70a5d9f30d57537e47a7c0b197
Vasili2797 <nashvilliv1@gmail.com>
2024-03-04 02:50:14 -0500

    fixed the spacing

--- shoppingListApp/shopping/src/Components/PageHeader.jsx
+++ shoppingListApp/shopping/src/Components/PageHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageHeader = () => {
   return (
     <>
-      <div style={{textAlign: "center"}}>
+      <div style={{ textAlign: "center" }}>
         <h1>Thank You for visiting our shopping list app</h1>
         <h3>Please Add items on the bottom of the link</h3>
       </div>
